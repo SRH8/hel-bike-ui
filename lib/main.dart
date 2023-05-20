6d33aa970498eb80d77a0b30b100db1eadc7bd06
Sergio Fraga <sergiofraga.cs@gmail.com>
2023-05-20 16:49:09 +0200

    Install Open Sans fonts and http package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hel_bike_ui/screens/home_screen.dart';
+import 'package:hel_bike_ui/utils/application_constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: ApplicationConstants.appTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: Theme.of(context).textTheme.apply(
+          fontFamily: 'Open Sans'
+        )
       ),
       home: const HomeScreen(),
     );
