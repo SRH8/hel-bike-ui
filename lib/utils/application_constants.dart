3d07009078aa52f76ffa33250dfe605b64cf4bcd
Sergio Fraga <sergiofraga.cs@gmail.com>
2023-05-20 17:08:21 +0200

    Create application details widget to show the title and description of the application

---
+++ lib/utils/application_constants.dart
@@ -0,0 +1,12 @@
+abstract class ApplicationConstants {
+
+  static const String appDescription = "Welcome to Helsinki Bike App! Here you will find Helsinki's and Espoo's bike stations, as well as information "
+      "about the journeys taken in the summer of 2021";
+
+  static const String bikeStationImgCredits = 'City bikes and kick scooters at the Central Station in Helsinki,'
+      ' Finland, 2019 - by Anneli Vaniala is licensed under CC BY 4.0.';
+
+  static const String appTitle = 'Helsinki Bike App';
+
+  static const String bikeStationImg = 'assets/images/bike_station.jpg';
+}
---
+++ lib/widgets/application_details/application_details.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hel_bike_ui/utils/application_constants.dart';
+import 'package:hel_bike_ui/utils/styles.dart';
+
+class ApplicationDetails extends StatelessWidget {
+  const ApplicationDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 600,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            ApplicationConstants.appTitle,
+            style: ThemeText.appTitle
+          ),
+          SizedBox(height: 30),
+          Text(
+            ApplicationConstants.appDescription,
+            style: ThemeText.appDescription
+          )
+        ],
+      ),
+    );
+  }
+}
