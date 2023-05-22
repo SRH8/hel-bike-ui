81dc4aeb838f205e656672ddd64ecd4bef2f4a35
Sergio Fraga <sergiofraga.cs@gmail.com>
2023-05-22 17:29:57 +0200

    Create service to fetch stations from the server

---
+++ lib/service/station_service.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hel_bike_ui/model/station.dart';
+import 'package:hel_bike_ui/model/station_response.dart';
+import 'package:http/http.dart' as http;
+
+class StationService {
+
+  final String _baseUrl = dotenv.get('BASE_URL');
+
+  Future<List<Station>> getStationsData() async {
+    List<Station> stations = [];
+    int currentPage = 0;
+    Uri uri = Uri.http(_baseUrl, 'api/v1/stations', {'pageNo': '$currentPage'});
+
+    final response = await http.get(uri);
+
+    if(response.statusCode == 200) {
+      StationResponse stationResponse = stationResponseFromJson(response.body);
+      stations = stationResponse.content;
+
+      return stations;
+    }
+    return stations;
+  }
+}
