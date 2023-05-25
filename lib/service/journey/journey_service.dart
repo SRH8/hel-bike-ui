3c798a9cfead4cf0112ecd3d00a20d7eba6373ce
Sergio Fraga <sergiofraga.cs@gmail.com>
2023-05-25 17:26:16 +0200

    Create journey service to fetch journeys from the server

---
+++ lib/service/journey/journey_service.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hel_bike_ui/model/journey/journey_response.dart';
+import 'package:http/http.dart' as http;
+import 'package:hel_bike_ui/model/journey/journey.dart';
+
+class JourneyService {
+
+  final String _baseUrl = dotenv.get('BASE_URL');
+  final int _pageSize = 2000;
+
+  Future<List<Journey>> getJourneys() async {
+    List<Journey> journeys = [];
+
+    Uri uri = Uri.http(_baseUrl, 'api/v1/journeys', {'pageSize': '$_pageSize'});
+
+    final response = await http.get(uri);
+
+    if(response.statusCode == 200) {
+      JourneyResponse journeyResponse = journeyResponseFromJson(response.body);
+      journeys = journeyResponse.content;
+
+      return journeys;
+    }
+    return journeys;
+  }
+}
